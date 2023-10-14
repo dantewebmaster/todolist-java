32c527427364260f5e60519b906f6196b572f3a6
Dante <dante.roberio@g.globo>
2023-10-14 14:39:11 -0300

    initial version 1.0.0

---
+++ src/main/java/br/com/dantewebmaster/todolist/task/TaskController.java
@@ -0,0 +1,86 @@
+package br.com.dantewebmaster.todolist.task;
+
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.UUID;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import br.com.dantewebmaster.todolist.utils.Utils;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.websocket.server.PathParam;
+
+@RestController
+@RequestMapping("/tasks")
+public class TaskController {
+  @Autowired
+  private ITaskRepository taskRepository;
+
+  @PostMapping("/")
+  public ResponseEntity create(@RequestBody TaskModel taskModel, HttpServletRequest request) {
+    var userId = request.getAttribute("user_id");
+    taskModel.setUserId((UUID) userId);
+
+    var currentDate = LocalDateTime.now();
+    var isInDateRange = currentDate.isAfter(taskModel.getStart_at()) || currentDate.isAfter(taskModel.getEnd_at());
+
+    if (isInDateRange) {
+      return ResponseEntity
+          .status(HttpStatus.BAD_REQUEST)
+          .body("A data de início/término deve ser maior que a data atual.");
+    }
+
+    var isStartDateAfterEndDate = taskModel.getStart_at().isAfter(taskModel.getEnd_at());
+
+    if (isStartDateAfterEndDate) {
+      return ResponseEntity
+          .status(HttpStatus.BAD_REQUEST)
+          .body("A data de início deve ser menor que a data de término.");
+    }
+
+    var createdTask = this.taskRepository.save(taskModel);
+
+    return ResponseEntity.status(HttpStatus.OK).body(createdTask);
+  }
+
+  @GetMapping("/")
+  public List<TaskModel> list(HttpServletRequest request) {
+    var userId = request.getAttribute("user_id");
+    var userTasks = this.taskRepository.findByUserId((UUID) userId);
+
+    return userTasks;
+  }
+
+  @PutMapping("/{id}")
+  public ResponseEntity update(
+      @RequestBody TaskModel taskModel,
+      HttpServletRequest request,
+      @PathVariable UUID id) {
+    var task = this.taskRepository.findById(id).orElse(null);
+
+    if (task == null) {
+      return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Tarefa não encontrada");
+    }
+
+    var userId = request.getAttribute("user_id");
+
+    if (!task.getUserId().equals(userId)) {
+      return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Usuário não tem permissão para alterar essa tarefa");
+    }
+
+    Utils.copyNonNullProperties(taskModel, task);
+
+    var updatedTask = this.taskRepository.save(task);
+
+    return ResponseEntity.ok().body(updatedTask);
+  }
+}
